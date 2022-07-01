94a82fd7778bdaf592bfa24c07c7e0a40c64dab3
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-07-01 19:23:25 -0400

    WIP: init commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "day-primary": "#00668A",
+        "day-secondary": "#004E71",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+};
